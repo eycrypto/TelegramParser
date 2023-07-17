9fa48ff93ff45f8a103fec1a5ba097658ca41bdd
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2023-07-17 17:34:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import csv
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.functions.users import GetFullUserRequest
 
-# Считываем учетные данные
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-# Присваиваем значения внутренним переменным
 api_id   = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
