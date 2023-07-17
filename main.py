6d0d1089299c2ba0aa9a666e3344f8fa8a88197f
Skat100500 <slava2006tukin@mail.ru>
2023-07-17 18:43:53 +0700

    TGParser upd 18.07.2023

--- main.py
+++ main.py
@@ -1,69 +1,83 @@
 import configparser
-import json
+import os
+import django
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Models.settings'
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
 
+from tg_bot_models.models import URLModels, Users
 from telethon.sync import TelegramClient
-import csv
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.functions.users import GetFullUserRequest
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-api_id   = config['Telegram']['api_id']
+api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
-
-
 
 client = TelegramClient(username, api_id, api_hash)
 
 client.start()
 
+
 async def get_full(id):
-	return await client(GetFullUserRequest(id))
+    return await client(GetFullUserRequest(id))
 
-async def get_users(channel):
-	offset_msg = 0
-	limit_msg = 100
-	users = {}
-	all_messages = 0
-	total_user_limit = int(input('Сколько ников пользователей вам нужно получить? '))
 
-	while len(users) < total_user_limit:
-		history = await client(GetHistoryRequest(
-			peer=channel,
-			offset_id=offset_msg,
-			offset_date=None, add_offset=0,
-			limit=limit_msg, max_id=0, min_id=0,
-			hash=0))
-		if not history.messages:
-			break
-		messages = history.messages
-		for message in messages:
-			all_messages +=1
-			try:
-				id = message.to_dict()['from_id']['user_id']
-				if id not in users:
-					full = await get_full(id)
-					users[id] = full.users[0].username
-				if len(users)>=total_user_limit:
-					break
-			except KeyError:
-				pass
-		offset_msg = messages[len(messages) - 1].id
-	with open('user.csv', 'w') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow(('ID', 'USERNAME'))
-		for i in users:
-			writer.writerow((i, users[i]))
+async def get_users(channel, model_url):
+    print(f'Start parsing {model_url.url}')
+    offset_msg = 0
+    limit_msg = 100
+    users = Users.objects.all()
+    finish_check_message = True
+    a = True
+
+    while finish_check_message:
+        history = await client(GetHistoryRequest(
+            peer=channel,
+            offset_id=offset_msg,
+            offset_date=None, add_offset=0,
+            limit=limit_msg, max_id=0, min_id=0,
+            hash=0))
+        if not history.messages:
+            break
+        messages = history.messages
+        if not model_url.inuse:
+            model_url.inuse = True
+            model_url.last_message = messages[0].id - 5000
+            model_url.save()
+        elif model_url.inuse and a:
+            model_url.last_message = messages[0].id + 1
+            model_url.save()
+            a = False
+        for message in messages:
+            try:
+                id = message.to_dict()['from_id']['user_id']
+                if not users.filter(user_id=id):
+                    full = await get_full(id)
+                    username = full.users[0].username
+                    Users.objects.create(user_id=id,
+                                         username=username,
+                                         find_chat=model_url.url)
+                    if message.id == model_url.last_message:
+                        finish_check_message = False
+                        break
+            except KeyError:
+                pass
+        offset_msg = messages[-1].id
 
 
 async def main():
-	url = 'https://t.me/CryptoBotRussian'
-	channel = await client.get_entity(url)
-	await get_users(channel)
+    while True:
+        urls = URLModels.objects.all()
+        for model_url in urls:
+            url = model_url.url
+            channel = await client.get_entity(url)
+            await get_users(channel, model_url)
 
 
 with client:
-	client.loop.run_until_complete(main())
+    client.loop.run_until_complete(main())
